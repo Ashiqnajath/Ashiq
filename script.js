94ab83098dcb1d4d2ff760ec56fc82a01bd6e3d4
Ashiqnajath <ashiksadhamukku@gmail.com>
2020-06-16 16:58:19 +0400

    replace live chat

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5ee8adda9e5f69442290aaee/default';
+s1.src='https://embed.tawk.to/5ee8adda9e5f69442290aaee/1eauj50lu';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
