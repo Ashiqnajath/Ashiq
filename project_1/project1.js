c8fe465235cc079ffebdd46dbd0ffe4d5a74a373
Ashiqnajath <ashiksadhamukku@gmail.com>
2020-06-16 16:39:19 +0400

    add live chat and template download button

--- project_1/project1.js
+++ project_1/project1.js
@@ -13,3 +13,13 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 800,
 	speedAsDuration: true
 });
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ee8adda9e5f69442290aaee/1eauf27tm';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
